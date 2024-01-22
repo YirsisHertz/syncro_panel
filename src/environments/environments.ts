48e1011dfa7e4896a4c1489f0b6b63a3b6e64d83
Yirsis Serrano <yirsishertz02@gmail.com>
2024-01-22 12:22:21 -0600

    fix(temp): change conection

--- src/environments/environments.ts
+++ src/environments/environments.ts
@@ -8,5 +8,5 @@ export const environment: Environment = {
     default_image:
       "https://images.unsplash.com/photo-1703209935484-34f5c826f1da?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w1NTUxNDN8MHwxfHJhbmRvbXx8fHx8fHx8fDE3MDU3MjE5MjB8&ixlib=rb-4.0.3&q=80&w=1080"
   },
-  webcard_base_url: "http://localhost:3000"
+  webcard_base_url: "http://192.168.100.30:3000"
 };
